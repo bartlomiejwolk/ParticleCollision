682bfe2f6f171ad83f6dba4ce9970d48cc805a0d
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-28 15:07:04 +0200

    Initial commit

---
+++ Editor/ParticleCollisionEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+
+namespace ParticleCollisionEx {
+
+    [CustomEditor(typeof(ParticleCollision))]
+    [CanEditMultipleObjects]
+    public sealed class ParticleCollisionEditor : Editor {
+        #region FIELDS
+
+        private ParticleCollision Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (ParticleCollision)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    ParticleCollision.Version,
+                    ParticleCollision.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/ParticleCollision")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(ParticleCollision));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
---
+++ ParticleCollision.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+namespace ParticleCollisionEx {
+
+    public sealed class ParticleCollision : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "ParticleCollision";
+
+        #endregion
+
+        #region DELEGATES
+        #endregion
+
+        #region EVENTS
+        #endregion
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "ParticleCollision";
+#pragma warning restore0414
+
+        #endregion
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        ///     Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        private void OnCollisionEnter(Collision collision) { }
+
+        private void OnCollisionStay(Collision collision) { }
+
+        private void OnCollisionExit(Collision collision) { }
+
+        #endregion
+
+        #region EVENT INVOCATORS
+        #endregion
+
+        #region EVENT HANDLERS
+        #endregion
+
+        #region METHODS
+        #endregion
+
+    }
+
+}
